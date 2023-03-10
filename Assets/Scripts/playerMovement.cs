806e0b816116fe14fc2ad973c75f068c82e2ffe8
Anima <anis.mejri@live.de>
2023-03-10 22:40:21 +0100

    player shoooooot

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -15,7 +15,8 @@ public class playerMovement : MonoBehaviour
         Move();
     }
 
-    void Move() {
+    void Move()
+    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
@@ -25,11 +26,20 @@ public class playerMovement : MonoBehaviour
         // Normalize the movement vector to prevent faster diagonal movement
         movementVector = movementVector.normalized;
 
+        // Get the mouse position in world space
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+
+        // Calculate the direction vector from the player to the mouse
+        Vector3 direction = (mousePosition - transform.position).normalized;
+
+        // Rotate the player to face the mouse position
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+
         // Move the player in the movement direction
         transform.position += new Vector3(movementVector.x, movementVector.y, 0f) * moveSpeed * Time.deltaTime;
-
-        
     }
+
 
 
 }
