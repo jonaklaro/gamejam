fcf9c7eb1cb4fc55c86240ec06d973dd12267dd8
qwertgo <127448030+qwertgo@users.noreply.github.com>
2023-03-10 21:28:18 +0100

    First Basic Implementation
    joa funktioniert halt

---
+++ Assets/Scenes/Samu/Scripts/PlayerHealthTimer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthTimer : MonoBehaviour
+{
+    [SerializeField] float healthTimer = 30f;
+    [SerializeField] float healthTimerMax = 60f;
+    [SerializeField] Image healthBarSpriteLeft = null;
+    [SerializeField] Image healthBarSpriteRight = null;
+
+    void Update()
+    {
+        healthTimer -= Time.deltaTime;
+        healthBarSpriteLeft.fillAmount = healthTimer / healthTimerMax;
+        healthBarSpriteRight.fillAmount = healthBarSpriteLeft.fillAmount;
+
+        if (healthTimer < 0)
+            Die();
+    }
+
+    public void TakeDamage(float timeLost)
+    {
+        healthTimer -= timeLost;
+        if (healthTimer < 0)
+            Die();
+    }
+
+    public void GatherRessource(float timeGained)
+    {
+        healthTimer += timeGained;
+        healthTimer = Mathf.Min(healthTimer, healthTimerMax);
+    }
+
+    void Die()
+    {
+        Debug.Log("you ded");
+        enabled = false;
+    }
+}
