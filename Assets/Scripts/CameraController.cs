57c45c4c03f8ea871d635c568160574cb6d5a91f
qwertgo <127448030+qwertgo@users.noreply.github.com>
2023-03-11 12:30:36 +0100

    Erstes Level Design + Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    [SerializeField] float followDelay = 0.1f;
+
+    void Update()
+    {
+        float t =  1f - Mathf.Pow(followDelay, Time.deltaTime);
+        Vector3 newPos = Vector3.Lerp(transform.position, player.transform.position, t);
+        newPos.z = -10;
+        transform.position = newPos;
+    }
+}
