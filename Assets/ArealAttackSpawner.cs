c3c07ea8138c12fcf3846489df61847107c16d79
Marie <mplekat@hs-mittweida.de>
2023-03-11 23:24:37 +0100

    ArealSpawner Basics

--- Assets/ArealAttackSpawner.cs
+++ Assets/ArealAttackSpawner.cs
@@ -1,18 +1,74 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ArealAttackSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] spawningPoint;
 
+    [Header("Timercounts")] [SerializeField]
+    private float timeTillCollider;
+
+    [SerializeField] private float range;
+    private GameObject player;
+
+    [SerializeField] private ParticleSystem arealEffect;
+    
+
     private void Start()
     {
+        player = GameObject.FindWithTag("Player");
         spawningPoint = new GameObject[13];
         for (int i = 0; i < spawningPoint.Length; i++)
         {
             spawningPoint[i].SetActive(false);
         }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < spawningPoint.Length; i++)
+        {
+            Vector2 spawnPos = new Vector2(spawningPoint[i].transform.position.x, spawningPoint[i].transform.position.y);
+            float distance = GetDistance(spawnPos);
+
+            if (distance < range)
+            {
+                spawningPoint[i].SetActive(true);
+                ActivateArea(spawningPoint[i]);
+            }
+            
+        }
+    }
+
+    private void ActivateArea(GameObject spawnPoint)
+    {
+        
+       //Arealeffect starten
+    }
+
+    private float GetDistance(Vector2 spawningPoint)
+    {
+        float distance = Vector2.Distance(spawningPoint, player.transform.position);
+        return distance;
+    }
+
+    private IEnumerator TurnColliderON()
+    {
+        yield return new WaitForSeconds(timeTillCollider);
+        TurnColliderON();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            MakeDamage();
+        }
+    }
+
+    private void MakeDamage()
+    {
+        
     }
 }
