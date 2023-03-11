08fd382bd668e3ec9f669a22aa8c51cdc3834e90
qwertgo <127448030+qwertgo@users.noreply.github.com>
2023-03-11 13:34:49 +0100

    updated playerMovement

--- Assets/Scripts/Player/playerMovement.cs
+++ Assets/Scripts/Player/playerMovement.cs
@@ -6,55 +6,58 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-  public float moveSpeed = 5f;
-  [SerializeField] Animator animator;
-  Rigidbody2D rb;
-  SoundManager soundManager;
+    public float moveSpeed = 5f;
+    [SerializeField] Animator animator;
+    Rigidbody2D rb;
+    SoundManager soundManager;
 
 
-  private void Start()
-  {
-    rb = GetComponent<Rigidbody2D>();
-    soundManager = SoundManager.instance;
-    soundManager.SetVolume("MasterVolume", -40f);
-    soundManager.PlayMusic("8Bit2");
-  }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        soundManager = SoundManager.instance;
+        soundManager.SetVolume("MasterVolume", -40f);
+        soundManager.PlayMusic("8Bit2");
+    }
 
-  private void Update()
-  {
-    // Get the horizontal and vertical input axis
-    Move();
-  }
+    private void Update()
+    {
+        // Get the horizontal and vertical input axis
+        Move();
+    }
 
-  void Move()
-  {
-    float horizontalInput = Input.GetAxisRaw("Horizontal");
-    float verticalInput = Input.GetAxisRaw("Vertical");
+    void Move()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
 
-    // Create a movement vector from the input axis
-    Vector2 movementVector = new Vector2(horizontalInput, verticalInput);
+        // Create a movement vector from the input axis
+        Vector2 movementVector = new Vector2(horizontalInput, verticalInput);
 
-    // Normalize the movement vector to prevent faster diagonal movement
-    movementVector = movementVector.normalized;
+        // Normalize the movement vector to prevent faster diagonal movement
+        movementVector = movementVector.normalized;
 
-    // Get the mouse position in world space
-    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    mousePosition.z = 0f;
+        // Get the mouse position in world space
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
 
-    // Calculate the direction vector from the player to the mouse
-    Vector3 direction = (mousePosition - transform.position).normalized;
+        // Calculate the direction vector from the player to the mouse
+        Vector3 direction = (mousePosition - transform.position).normalized;
 
-    // Rotate the player to face the mouse position
-    //transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        // Rotate the player to face the mouse position
+        //transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
 
-    // Move the player in the movement direction using Rigidbody2D velocity
-    rb.velocity = movementVector * moveSpeed;
+        // Move the player in the movement direction using Rigidbody2D velocity
+        rb.velocity = movementVector * moveSpeed;
 
-    animator.SetFloat("Horizontal", movementVector.x);
-    animator.SetFloat("Vertical", movementVector.y);
-    animator.SetFloat("Speed", movementVector.sqrMagnitude);
+        //Rotation Lock
+        rb.freezeRotation = true;
 
-  }
+        animator.SetFloat("Horizontal", movementVector.x);
+        animator.SetFloat("Vertical", movementVector.y);
+        animator.SetFloat("Speed", movementVector.sqrMagnitude);
+
+    }
 
     public void Die()
     {
